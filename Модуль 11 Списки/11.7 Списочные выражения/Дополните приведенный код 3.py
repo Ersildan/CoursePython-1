37ec917e1d34e14cc1cb6e0215bbab20c72a759a
Andrey Svistunov <120823381+Ersildan@users.noreply.github.com>
2023-11-05 20:34:06 +0300

    Create Дополните приведенный код 3.py

---
+++ Модуль 11 Списки/11.7 Списочные выражения/Дополните приведенный код 3.py
@@ -0,0 +1,5 @@
+x = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+
+new_keywords = [m[:] for m in x if len(m) >= 5]
+
+print(new_keywords)
