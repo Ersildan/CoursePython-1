6460a55bdf5a28650db5fda6e2521ac112bf6af2
Andrey Svistunov <120823381+Ersildan@users.noreply.github.com>
2023-11-13 21:13:37 +0300

    task stepik
    Напишите функцию merge(list1, list2), которая принимает в качестве аргументов два отсортированных по возрастанию списка, состоящих из целых чисел, и объединяет их в один отсортированный список.

---
+++ Модуль 13 Функции/13.4 Функции с возвратом значения. Часть 1/Merge lists 1.py
@@ -0,0 +1,11 @@
+# объявление функции
+def merge(list1, list2):
+    a = numbers1 + numbers2
+    a.sort()
+    return a
+# считываем данные
+numbers1 = [int(c) for c in input().split()]
+numbers2 = [int(b) for b in input().split()]
+
+# вызываем функцию
+print(merge(numbers1, numbers2))
