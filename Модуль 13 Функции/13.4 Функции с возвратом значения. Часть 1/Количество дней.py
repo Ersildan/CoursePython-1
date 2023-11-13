04e9c8bb585243fb3b6ce989e569883c9df17978
Andrey Svistunov <120823381+Ersildan@users.noreply.github.com>
2023-11-13 21:11:05 +0300

    task stepik 13.4
    Напишите функцию get_days(month), которая принимает в качестве аргумента номер месяца и возвращает количество дней в данном месяце.

---
+++ Модуль 13 Функции/13.4 Функции с возвратом значения. Часть 1/Количество дней.py
@@ -0,0 +1,9 @@
+# объявление функции
+def get_days(month):
+    day = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+    return day[num]
+# считываем данные
+num = int(input())
+
+# вызываем функцию
+print(get_days(num))
