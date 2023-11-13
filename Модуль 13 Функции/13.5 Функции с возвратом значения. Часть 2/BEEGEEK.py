f7a6206b0325a534364a59ca6af1123baca48c0d
Andrey Svistunov <120823381+Ersildan@users.noreply.github.com>
2023-11-13 21:29:39 +0300

    edite file BEEGEEK
    push code

--- Модуль 13 Функции/13.5 Функции с возвратом значения. Часть 2/BEEGEEK.py
+++ Модуль 13 Функции/13.5 Функции с возвратом значения. Часть 2/BEEGEEK.py
@@ -1,6 +1,37 @@
+def is_palindrome(num):
+    return str(num) == str(num)[::-1]
+
+
+def is_prime(num):
+    if num == 1:
+        return False
+    
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+        
+    return True
+
+
+def is_even(num):
+    return num % 2 == 0
+    
+
+# объявление функции
 def is_valid_password(password):
-    pass
+    seq = password.split(":")
+    
+    if len(seq) == 3:
+        seq = [int(el) for el in seq]
+        a, b, c = seq[0], seq[1], seq[2]
+        
+        return is_palindrome(a) and is_prime(b) and is_even(c)
+    
+    return False
 
 
+# считываем данные
 psw = input()
+
+# вызываем функцию
 print(is_valid_password(psw))
